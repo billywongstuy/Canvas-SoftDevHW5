564744c6a0ed2ae068ab1d18c4d08713d29f74b5
Billy Wong <bwong5@stuy.edu>
2017-02-13 14:03:16 -0500

    nizche

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var c = document.getElementById("slate");
 var b = document.getElementById("clear");
 
-var crtRect = function() {
+var crtRect = function(e) {
     var ctx = c.getContext("2d");
     ctx.fillStyle = "#ff0000";
-    ctx.fillRect(50,50,100,200); 
+    ctx.fillRect(e.offsetX,e.offsetY,100,200);
+    console.log(e.offsetX);
+    console.log(e.offsetY);
 };
 
 c.addEventListener("click",crtRect);
@@ -13,7 +15,7 @@ c.addEventListener("click",crtRect);
 var clear = function() {
     var ctx = c.getContext("2d");
     ctx.fillStyle = "#ffffff";
-    ctx.fillRect(50,50,100,200); 
+    ctx.fillRect(0,0,c.width,c.height); 
 }
 
 b.addEventListener("click",clear);
